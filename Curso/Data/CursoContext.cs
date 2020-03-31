7d739b70205cabfdcfb4969341e12febceb5caeb
jego.novakosk <jnovakosk@gmail.com>
2020-03-31 15:43:00 -0300

    CRUD

---
+++ Curso/Data/CursoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Curso.Models;
+
+namespace Curso.Data
+{
+    public class CursoContext : DbContext
+    {
+        public CursoContext (DbContextOptions<CursoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Curso.Models.Departament> Departament { get; set; }
+    }
+}
--- Curso/Startup.cs
+++ Curso/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Curso.Data;
 
 namespace Curso
 {
@@ -33,6 +35,9 @@ namespace Curso
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<CursoContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("CursoContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
